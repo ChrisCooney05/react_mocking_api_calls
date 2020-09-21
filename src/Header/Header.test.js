902854d4a6c48573d3cdfa488047211a236cc2fd
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-21 11:53:38 +0100

    Able to test a mock api call using data stored from previous fetch

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,10 +1,18 @@
 import React, { Compornent } from "react";
-import { screen, render, waitFor } from "@testing-library/react";
-
+import { screen, render, waitForElement } from "@testing-library/react";
 import Header from "./Header";
+import mockResponse from "../__mocks__/github_response.json";
+
+// mock fetch and return the mock response json
+jest.spyOn(global, "fetch").mockImplementation(() => {
+  return Promise.resolve({
+    json: () => Promise.resolve(mockResponse),
+  });
+});
 
 test("Renders my name on the screen from api call", async () => {
   render(<Header />);
-  const myName = await waitFor(screen.getByText("Chris Cooney"));
+  const myName = await waitForElement(() => screen.getByText("Chris Cooney"));
+  //Should use waitFor instead of waitForElement but it requires updating testing libraries
   expect(myName).toBeInTheDocument();
 });
