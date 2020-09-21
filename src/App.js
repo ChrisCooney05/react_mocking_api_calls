26adce48fd4e558e54988002c83f32997b70e497
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-21 11:44:37 +0100

    Able to render user name, need to fix test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import Header from "./Header/Header.js";
 import "./App.css";
 
 function App() {
-  return <div className="App">Testing</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { options } from "../constants";
+
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInfo: [],
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      "https://api.github.com/users/ChrisCooney05",
+      options
+    );
+    const data = await res.json();
+    this.setState({ userInfo: data });
+  }
+
+  render() {
+    const { userInfo } = this.state;
+    return (
+      <div className="username">
+        <h1>{userInfo.name}</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
